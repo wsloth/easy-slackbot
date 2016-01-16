fca2f0191178e1f60e1b19d31137620b650d3e5d
Wessel Loth <wessel.loth@gmail.com>
2016-01-16 19:28:54 +0000

    Added loads of fixes, bot actually works now.

---
+++ examples.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var slack = require('./index');
+var SlackBot = slack.Bot;
+var Command = slack.Command;
+
+
+// Create your custom command
+class MyCmd extends Command {
+    constructor(commands) {
+        super(commands);
+    }
+
+    // Override the default handler (!)
+    handler(data, ctx, slack, callback) {
+        var response = 'Hello there!';
+        callback(response);
+    }
+}
+
+var bot = new SlackBot({
+    // API Auth token
+    token: '<your slack token>',
+    // Botname from @botmention
+    name: 'U0J1BG81G',
+    // Provide a onTeamJoin handler
+    welcome: function(data, ctx, callback) {
+        callback('Welcome!');
+    },
+    // Command prefix
+    prefix: '!',
+    // Pass any custom commands
+    commands: [
+        new MyCmd(['hi', 'hello', 'hey']),
+    ],
+});
+bot.connect();
--- index.js
+++ index.js
@@ -1,54 +1,57 @@
 'use strict';
 var Slack = require('slackbotapi');
 
-class SlackBot {
+function SlackBot(options) {
 
-    constructor(options) {
-        this.token = options.token;
-        this.prefix = options.prefix ? options.prefix : '!';
-        this.commands = options.commands ? options.commands : [];
-        this.switch = {};
+    var token = options.token;
+    var prefix = options.prefix ? options.prefix : '!';
+    var commands = options.commands ? options.commands : [];
+    var cmdswitch = {};
 
-        if (options.name)
-            this.name = options.name;
-        else if (process.env.SLACK_BOTNAME)
-            this.name = `<@${process.env.SLACK_BOTNAME}>`;
-        else
-            console.warn('No $SLACK_BOTNAME found. See the documentation.');
+    var name;
+    var welcome;
+    var api;
 
-        if (typeof options.welcome === 'function')
-            this.welcome = options.welcome;
-        else {
-            console.warn(`No welcome message handler found! Using default.`);
-            this.welcome = (dt, ctx, cb) => {
-                cb(`Hey there @${this.api.getUser(dt.user).name}! Welcome to the team!`);
-            };
-        }
+    if (options.name)
+        name = options.name;
+    else if (process.env.SLACK_BOTNAME)
+        name = `<@${process.env.SLACK_BOTNAME}>`;
+    else
+        console.warn('No $SLACK_BOTNAME found. See the documentation.');
+
+    if (typeof options.welcome === 'function')
+        welcome = options.welcome;
+    else {
+        console.warn(`No welcome message handler found! Using default.`);
+        welcome = (dt, ctx, cb) => {
+            cb(`Hey there @${api.getUser(dt.user).name}! Welcome to the team!`);
+        };
     }
 
-    addCommand(command) {
+    var addCommand = function (command) {
         if (command instanceof Command)
-            this.commands.push(command);
+            commands.push(command);
         else
             throw new Error('You must give an instance of a Command object.');
-    }
+    };
 
     // PRIVATE METHODS ====================================================== #
 
-    _onMessage(data) {
+    var _onMessage = function (data) {
         if (typeof data.text == 'undefined') return;
 
         // Received a command
-        var isValidMention = data.text.startsWith(this.name);
-        if (data.text.charAt(0) === this.prefix || isValidMention) {
+        var isValidMention = data.text.startsWith(name);
+        if (data.text.charAt(0) === prefix || isValidMention) {
+            console.log('Command message received.');
 
             // Split the command and it's arguments into an array
             var command;
             if (isValidMention) {
-                command = data.text.substring(this.name.length).split(' ');
+                command = data.text.substring(name.length).split(' ');
                 command.splice(0, 1);
 
-                if (command[0].startsWith(this.prefix))
+                if (command[0].startsWith(prefix))
                     command[0] = command[0].substring(1);
             }
             else
@@ -63,9 +66,11 @@ class SlackBot {
 
             // Respond if the command is in the commands array
             var cmd = command[0].toLowerCase();
-            if (typeof this.commands[cmd] == 'function') {
-                this.commands[cmd](data, command, this, function(res) {
-                    this.api.sendMsg(data.channel, res);
+            console.log(cmd);
+            console.log(cmdswitch);
+            if (typeof cmdswitch[cmd] == 'function') {
+                cmdswitch[cmd](data, command, this, function (res) {
+                    api.sendMsg(data.channel, res);
                 });
             }
             else {
@@ -75,47 +80,54 @@ class SlackBot {
                 // handle that here
             }
         }
-    }
+    };
 
 
     // API SETUP ============================================================ #
 
-    connect() {
+    var connect = function () {
+        cmdswitch = {};
         // Construct the commands switch
-        for (let command in this.commands) {
-            for (let cmd in command) {
-                if (this.switch[cmd])
+        for (var command in commands) {
+            for (var cmd of commands[command]) {
+                if (cmdswitch[cmd])
                     throw new Error(`Command "${cmd}" already exists.`);
-                else
-                    this.switch[cmd] = command.handler;
+                else {
+                    cmdswitch[cmd] = commands[command].handler;
+                }
             }
         }
-
         // Connect to the API
-        this.api = new Slack({
-            'token': this.token,
+        api = new Slack({
+            'token': token,
             'logging': true,
             'autoReconnect': true,
         });
 
-        this.api.on('message', this._onMessage);
-        this.api.on('team_join', this.welcome);
-    }
+        api.on('message', _onMessage);
+        api.on('team_join', welcome);
+    };
 
     // PUBLIC METHODS ======================================================= #
 
-    sendMsg(channel, message) {
-        this.api.sendMsg(channel, message);
-    }
+    var sendMsg = function (channel, message) {
+        api.sendMsg(channel, message);
+    };
 
-    getUser(id) {
-        return this.api.getUser(id);
-    }
+    var getUser = function (id) {
+        return api.getUser(id);
+    };
 
+    return {
+        getUser: getUser,
+        sendMsg: sendMsg,
+        connect: connect,
+        addCommand: addCommand,
+    };
 }
 
 class Command {
-    
+
     constructor(commands) {
         if (Object.prototype.toString.call(commands) === '[object Array]')
             this.commands = commands;
@@ -133,7 +145,6 @@ class Command {
         throw new Error('You must override the default handler() method.');
     }
 }
-
 
 module.exports.Bot = SlackBot;
 module.exports.Command = Command;
